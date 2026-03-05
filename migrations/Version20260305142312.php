a4943ffa9e01f0ab1e85d0f1a3862dfbe8f86dcb
Sam@Unique <aymantheremkus11@gmail.com>
2026-03-05 18:02:12 +0300

    I forgot to version this file; Make migration for quantity

---
+++ migrations/Version20260305142312.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260305142312 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE cart_item ADD COLUMN quantity INTEGER NOT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TEMPORARY TABLE __temp__cart_item AS SELECT id, cart_id, product_id FROM cart_item');
+        $this->addSql('DROP TABLE cart_item');
+        $this->addSql('CREATE TABLE cart_item (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, cart_id INTEGER NOT NULL, product_id INTEGER NOT NULL, CONSTRAINT FK_F0FE25271AD5CDBF FOREIGN KEY (cart_id) REFERENCES cart (id) NOT DEFERRABLE INITIALLY IMMEDIATE, CONSTRAINT FK_F0FE25274584665A FOREIGN KEY (product_id) REFERENCES product (id) NOT DEFERRABLE INITIALLY IMMEDIATE)');
+        $this->addSql('INSERT INTO cart_item (id, cart_id, product_id) SELECT id, cart_id, product_id FROM __temp__cart_item');
+        $this->addSql('DROP TABLE __temp__cart_item');
+        $this->addSql('CREATE INDEX IDX_F0FE25271AD5CDBF ON cart_item (cart_id)');
+        $this->addSql('CREATE INDEX IDX_F0FE25274584665A ON cart_item (product_id)');
+    }
+}
